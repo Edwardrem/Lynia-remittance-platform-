7f25ee1ecb6c066063d1e7f2352a3a5b39fb0a21
Rufaro Edward Mutepfa <rufaromutepfa@gmail.com>
2023-08-19 13:50:15 +0200

    Successfully intialised postgres database

--- remittance_platform/remittance_platform/settings.py
+++ remittance_platform/remittance_platform/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'remittance_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'remittance',
+        'USER': 'postgres',
+        'PASSWORD': 'remail',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
