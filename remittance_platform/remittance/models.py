483517d1f2c4008de8b9e3de07e5497064250f98
Rufaro Edward Mutepfa <45569487+Edwardrem@users.noreply.github.com>
2023-09-15 08:47:03 +0200

    fixed issues with transection status error , moving on to password error

--- remittance_platform/remittance/models.py
+++ remittance_platform/remittance/models.py
@@ -31,6 +31,7 @@ class Transaction(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.CharField(max_length=3)
     status = models.CharField(max_length=20)
+    transaction_status = models.CharField(max_length=255, blank=True, null=True)
 
 
 class TransactionHistory(models.Model):
