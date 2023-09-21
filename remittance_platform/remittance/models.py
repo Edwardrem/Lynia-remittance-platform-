dd5941bf31a95076eaea9261d47c9e36432520c9
Rufaro Edward Mutepfa <45569487+Edwardrem@users.noreply.github.com>
2023-09-21 21:54:30 +0200

    succesifully fixedresidual errors

--- remittance_platform/remittance/models.py
+++ remittance_platform/remittance/models.py
@@ -1,14 +1,12 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser, Group, Permission
 
 
-class User(models.Model):
-    username = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+class User(AbstractUser):
     phone_number = models.CharField(max_length=10)
+    groups = models.ManyToManyField(Group, through='User_groups', related_name='users')
+    user_permissions = models.ManyToManyField(Permission, through='User_user_permissions', related_name='users')
 
 
 class Transaction(models.Model):
@@ -21,19 +19,43 @@ class Transaction(models.Model):
 
 
 class TransactionHistory(models.Model):
-    sender = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='transactionhistory_senders',
-    )
-    recipient = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='transactionhistory_recipients',
-    )
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    currency = models.CharField(max_length=3)
-    status = models.CharField(max_length=10)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='transaction_histories')
+    timestamp = models.DateTimeField(auto_now_add=True)
 
+
+class LogEntry(models.Model):
+    action_time = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='log_entries')
+    content_type = models.ForeignKey('contenttypes.ContentType', on_delete=models.CASCADE, blank=True, null=True, related_name='log_entries')
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField(blank=True)
+
+
+class ContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        unique_together = (('app_label', 'model'),)
+
+    def __str__(self):
+        return self.model
+
+
+class User_groups(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'remittance_user_groups'
+
+
+class User_user_permissions(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'remittance_user_user_permissions'
