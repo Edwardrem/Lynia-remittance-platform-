4e20b454be03884ebb272e9bcef4077e66c3972e
Rufaro Edward Mutepfa <45569487+Edwardrem@users.noreply.github.com>
2023-09-21 21:05:30 +0200

    added github copilot to try and fix errors

--- remittance_platform/remittance/forms.py
+++ remittance_platform/remittance/forms.py
@@ -55,6 +55,12 @@ class LoginForm(forms.Form):
     
 
 class RegisterForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    new_password1 = forms.CharField(widget=forms.PasswordInput)
+    new_password2 = forms.CharField(widget=forms.PasswordInput)
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'new_password1', 'new_password2']
